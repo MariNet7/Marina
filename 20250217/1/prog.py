1edbee3021dbeb5e4e8c990076c4ee52a6048588
MariNet7 <mvd172428@gmail.com>
2025-02-18 19:27:24 +0300

    Dobavila pokaz commita

--- 20250217/1/prog.py
+++ 20250217/1/prog.py
@@ -14,6 +14,23 @@ def list_branches(repo_path):
         print(branch.name)
 
 
+def show_commit(repo_path, branch):
+    git_dir = Path(repo_path) / ".git"
+    br_path = git_dir / "refs/heads" / branch
+    if not br_path.exists():
+        raise ValueError(f"Branch {branch} not found")
+
+    with open(br_path, "r") as f:
+        commit_hash = f.read().strip()
+
+    obj_path = git_dir / "objects" / commit_hash[:2] / commit_hash[2:]
+    with open(obj_path, "rb") as f:
+        commit = zlib.decompress(f.read())
+
+    header, _, body = commit.partition(b'\x00')
+    print(body.decode())
+
+
 if __name__ == "__main__":
 
     repo_path = sys.argv[1]
