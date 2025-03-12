7b3d730b53b96ad5c7e8c17e92574d44ed48d59b
MariNet7 <mvd172428@gmail.com>
2025-03-12 21:25:03 +0300

    Dobavila commandnuyu stroku

--- 20250310/1/prog.py
+++ 20250310/1/prog.py
@@ -1,10 +1,12 @@
-import sys
-from io import StringIO
-import cowsay
 import shlex
+import sys
+import cowsay
+from io import StringIO
+import cmd
+
 
 player_position = (0, 0)
-game_map = [(10 * [None]) for _ in range(10)]
+game_map = [ (10 * [None]) for _ in range(10) ]
 zluchki = {"jgsbat": cowsay.read_dot_cow(StringIO(r"""
     $the_cow = <<EOC;
              $thoughts
@@ -20,7 +22,7 @@ zluchki = {"jgsbat": cowsay.read_dot_cow(StringIO(r"""
              (((""  "")))
     EOC
     """))
-           }
+}
 
 
 class Kitty:
@@ -59,64 +61,66 @@ def encounter():
 
     if kitty.name in cowsay.list_cows():
         print(cowsay.cowsay(game_map[player_position[1]][player_position[0]].meow,
-                            cow=cowsay.cowsay(game_map[player_position[1][player_position[0]]].name)))
+                        cow=cowsay.cowsay(game_map[player_position[1][player_position[0]]].name)))
     else:
         print(cowsay.cowsay(game_map[player_position[1]][player_position[0]].meow,
                             cowfile=zluchki[kitty.name]))
 
 
-def main():
-    print("<<< Welcome to Python-MUD 0.1 >>>")
+class MUD(cmd.Cmd):
+    intro = '<<< Welcome to Python-MUD 0.1 >>>'
+    prompt = 'MUD>> '
 
-    for user_input in sys.stdin:
-        if not user_input.strip:
-            continue
+    def do_up(self, args):
+        move((0, -1))
 
+    def do_down(self, args):
+        move((0, 1))
+
+    def do_left(self, args):
+        move((-1, 0))
+
+    def do_right(self, args):
+        move((1, 0))
+
+    def do_addmon(self, arg):
         try:
-            args = shlex.split(user_input.strip())
+            args = shlex.split(arg.strip())
 
-            match args[0]:
-                case "up":
-                    move((0, -1))
-                case "down":
-                    move((0, 1))
-                case "right":
-                    move((1, 0))
-                case "left":
-                    move((-1, 0))
-                case "addmon":
-                    if len(args) != 9:
-                        raise ValueError
+            if len(args) != 9:
+                raise ValueError
 
-                    kitty_name = args[1]
-                    kitty_params = args[2:]
+            kitty_name = args[1]
+            kitty_params = args[2:]
 
-                    hp = None
-                    meow = None
-                    coords = None
-                    i = 0
-                    while i < len(kitty_params):
-                        if kitty_params[i] == 'hello':
-                            meow = kitty_params[i + 1]
-                            i += 2
-                        elif kitty_params[i] == 'hp':
-                            hp = int(kitty_params[i + 1])
-                            i += 2
-                        elif kitty_params[i] == 'coords':
-                            coords = (int(kitty_params[i + 1]), int(kitty_params[i + 2]))
-                            i += 3
-                        else:
-                            raise ValueError("Unknown parameter")
+            hp = None
+            meow = None
+            coords = None
+            i = 0
+            while i < len(kitty_params):
+                if kitty_params[i] == 'hello':
+                    meow = kitty_params[i + 1]
+                    i += 2
+                elif kitty_params[i] == 'hp':
+                    hp = int(kitty_params[i + 1])
+                    i += 2
+                elif kitty_params[i] == 'coords':
+                    coords = (int(kitty_params[i + 1]), int(kitty_params[i + 2]))
+                    i += 3
+                else:
+                    raise ValueError("Unknown parameter")
 
-                    if None in (meow, coords, hp):
-                        raise ValueError("Missing parameter")
+            if None in (meow, coords, hp):
+                raise ValueError("Missing parameter")
 
-                    add_monster(kitty_name, coords, meow, hp)
-                case _:
-                    print("Invalid command")
-        except:
+            add_monster(kitty_name, coords, meow, hp)
+
+        except Exception as e:
             print('Invalid arguments')
 
+
+def main():
+    MUD().cmdloop()
 
 if __name__ == "__main__":
     main()
