78119d3b02ccdf75213980613d53b01486a155de
MariNet7 <mvd172428@gmail.com>
2025-03-29 20:02:38 +0300

    Dobavila multiplayer. Chto-to ne rabotaet

--- 20250324/1/client.py
+++ 20250324/1/client.py
@@ -3,31 +3,13 @@ import asyncio
 import cowsay
 from io import StringIO
 import cmd
+import sys
+import readline
+import threading
+
 
 urons = {'sword': 10, 'spear': 15, 'axe': 20}
-zluchki = {"jgsbat": cowsay.read_dot_cow(StringIO(r"""
-    $the_cow = <<EOC;
-             $thoughts
-              $thoughts
-        ,_                    _,
-        ) '-._  ,_    _,  _.-' (
-        )  _.-'.|\\\\--//|.'-._  (
-         )'   .'\\/o\\/o\\/'.   `(
-          ) .' . \\====/ . '. (
-           )  / <<    >> \\  (
-            '-._/`  `\\_.-'
-      jgs     \\\\'--'//
-             (((""  "")))
-    EOC
-    """))
-}
-
-
-def encounter(name, hello):
-    if name in cowsay.list_cows():
-        print(cowsay.cowsay(hello, cow = name))
-    else:
-        print(cowsay.cowsay(hello, cowfile=zluchki[name]))
+zluchki = ["jgsbat"]
 
 
 def antichaos_addmon(args):
@@ -93,59 +75,48 @@ class MUD(cmd.Cmd):
 
     def __init__(self):
         super().__init__()
-        self.loop = asyncio.get_event_loop()
-        self.reader = None
-        self.writer = None
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+        self.gamer = sys.argv[1]
+        self.lloop = None
+        self.lochered = None
+        self.stop = None
 
-    def preloop(self):
-        self.loop.run_until_complete(self.init_gamer())
+    def postcmd(self, stop, line):
+        if self.stop.is_set():
+            return True
+        return super().postcmd(stop, line)
 
-    async def init_gamer(self):
-        self.reader, self.writer = await asyncio.open_connection('localhost', 1111)
-
-    async def send(self, request):
-        self.writer.write((request + '\n').encode())
-        await self.writer.drain()
-        reply = await self.reader.readline()
-        return reply.decode().strip()
-
+    def send(self, vvod):
+        if self.lloop and self.lochered:
+            self.lloop.call_soon_threadsafe(self.lochered.put_nowait, vvod)
+        else:
+            exit(0)
 
     def do_up(self, arg):
-        self.loop.run_until_complete(self.move(0, -1))
+        self.move(0, -1)
 
     def do_down(self, arg):
-        self.loop.run_until_complete(self.move(0, 1))
+        self.move(0, 1)
 
     def do_left(self, arg):
-        self.loop.run_until_complete(self.move(-1, 0))
+        self.move(-1, 0)
 
     def do_right(self, arg):
-        self.loop.run_until_complete(self.move(1, 0))
+        self.move(1, 0)
 
     async def move(self, dx, dy):
-        otvet = await self.send(f"move {dx} {dy}")
-        x, y, *kitty = shlex.split(otvet)
-        print(f'Moved to ({x}, {y})')
-        if kitty:
-            name, *meows = kitty
-            meow = ' '.join(meows)
-            encounter(name, meow)
+        self.send(f"move {dx} {dy}")
 
     def do_addmon(self, arg):
-        name, coords, meow, hp  = antichaos_addmon(arg)
+        name, (x, y), meow, hp  = antichaos_addmon(arg)
         if (name not in cowsay.list_cows()) and (name not in zluchki):
             print('Cannot add unknown monster')
             return
-        self.loop.run_until_complete(self._add_monster(name, meow, hp, coords))
-
-    async def _add_monster(self, name, meow, hp, coords):
-        otvet = await self.send(f"addmon {name} {coords[0]} {coords[1]} {hp} {meow}")
-        print(f'Added monster {name} to ({coords[0]}, {coords[1]}) saying {meow}')
-        if otvet == 'zamena':
-            print('Replaced the old monster')
+        self.send(f"addmon {name} {x} {y} {hp} {meow}")
 
     def complete_addmon(self, text):
-        kitties = cowsay.list_cows() + list(zluchki.keys())
+        kitties = cowsay.list_cows() + zluchki
         if len(kitties) < 3:
             return [name for name in kitties if name.startswith(text)]
         else:
@@ -153,22 +124,10 @@ class MUD(cmd.Cmd):
 
     def do_attack(self, arg):
         name, uron = antichaos_attack(arg)
-        self.loop.run_until_complete(self.attack(name, uron))
-
-    async def attack(self, name, uron):
-        otvet = await self.send(f"attack {name} {uron}")
-        if otvet:
-            uron, hp = otvet.split(' ', 1)
-            print(f'Attacked {name},  damage {uron} hp')
-            if hp == '0':
-                print(f'{name} died')
-            else:
-                print(f'{name} now has {hp}')
-        else:
-            print(f'No {name} here')
+        self.send(f"attack {name}, {uron}")
 
     def complete_attack(self, line, text):
-        kitties = cowsay.list_cows() + list(zluchki.keys())
+        kitties = cowsay.list_cows() + zluchki
         if len(line.split()) == 2:
             return [name for name in kitties if name.startswith(text)]
         elif len(line.split()) == 3:
@@ -176,8 +135,77 @@ class MUD(cmd.Cmd):
         else:
             return [name for name in list(urons.keys()) if name.startswith(text)]
 
+async def local_server(game):
+    try:
+        reader, writer = await asyncio.open_connection('localhost', 1111)
+    except Exception:
+        game.stop.set()
+        print('Server is busy')
+        exit(0)
+
+    writer.write(f'{sys.argv[1]}\n'.encode())
+    otvet = (await reader.readline()).decode().strip()
+
+    if otvet == 'busy_name':
+        game.stop.set()
+        print('Username is already taken!')
+        writer.close()
+        await writer.wait_closed()
+        exit(0)
+
+    otpravleno = asyncio.create_task(game.local_srv_queue.get())
+    polucheno = asyncio.create_task(reader.readline())
+
+    try:
+        while True:
+            sdelano, _ = await asyncio.wait(
+                [otpravleno, polucheno],
+                return_when=asyncio.FIRST_COMPLETED
+            )
+
+            for task in sdelano:
+                if task is otpravleno:
+                    data = task.result()
+                    writer.write(f"{data}\n".encode())
+                    await writer.drain()
+                    otpravleno = asyncio.create_task(game.local_srv_queue.get())
+
+                elif task is polucheno:
+                    otvet = task.result().decode().strip()
+                    if otvet == 'server_busy':
+                        game.stop.set()
+                        print('server is unavailable!')
+                        raise Exception('server is unavailable')
+
+                    print(f'\n{otvet.replace('\\n', '\n')}\n{game.prompt}{readline.get_line_buffer()}', end='',
+                          flush=True)
+                    polucheno = asyncio.create_task(reader.readline())
+
+    except Exception as e:
+        if e.args[0] != 'server is busy':
+            print(e)
+    finally:
+        otpravleno.cancel()
+        polucheno.cancel()
+        writer.close()
+        await writer.wait_closed()
+
+
+def play_game(game):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    game.lochered = asyncio.Queue()
+    game.lloop = loop
+    game.stop = threading.Event()
+    loop.run_until_complete(local_server(game))
+
 
 def main():
+    if len(sys.argv) < 2:
+        print('Input Username')
+        return
+    game = MUD()
+    threading.Thread(target=play_game, args=(game,)).start()
     MUD().cmdloop()
 
 if __name__ == "__main__":
